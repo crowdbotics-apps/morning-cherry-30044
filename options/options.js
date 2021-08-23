51e6cfab034e5282dd91c3a899bcae7f229e0290
Crowdbotics <team@crowdbotics.com>
2021-08-23 15:39:56 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "morning_cherry_30044Identifier",
+  url: "https://morning_cherry_30044Identifier.botics.co",
+  api: "https://morning_cherry_30044Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
